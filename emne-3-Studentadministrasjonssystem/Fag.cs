dd651039aa79333c8e86403c9081f412056543ae
griminir <viktor.elliot.king@gmail.com>
2024-05-11 01:58:51 +0200

    refactoret koden slik at 1 metode lager flere klasser

--- emne-3-Studentadministrasjonssystem/Fag.cs
+++ emne-3-Studentadministrasjonssystem/Fag.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,12 +13,39 @@ namespace emne_3_Studentadministrasjonssystem
         internal int _fagkode = fagkode;
         private string _fagnavn = fagnavn;
         internal int _studiepoeng = studiepoeng;
+        private static List<Fag> _faglist = LagFag();
 
         public void SkrivUtInfo()
         {
             Console.WriteLine($"fagkode: {_fagkode}");
             Console.WriteLine($"fagnavn: {_fagnavn}");
             Console.WriteLine($"Studiepoeng: {_studiepoeng}");
+        }
+
+        internal static List<Fag> LagFag()
+        {
+            List<Fag> Faglist = new List<Fag>();
+
+            Fag fag1 = new(301, "historie og filosofi", 30);
+            Fag fag2 = new(201, "historie", 15);
+            Fag fag3 = new(101, "filosofi", 15);
+            Fag fag4 = new(801, "programmering", 30);
+            Fag fag6 = new(303, "streetfigthing", 5);
+            Fag fag7 = new(505, "heimkunskap", 30);
+
+            Faglist.Add(fag1);
+            Faglist.Add(fag2);
+            Faglist.Add(fag3);
+            Faglist.Add(fag4);
+            Faglist.Add(fag6);
+            Faglist.Add(fag7);
+
+            return Faglist;
+        }
+
+        public static Fag GetFag(int number)
+        {
+            return _faglist[number];
         }
     }
 }
--- emne-3-Studentadministrasjonssystem/Program.cs
+++ emne-3-Studentadministrasjonssystem/Program.cs
@@ -7,21 +7,13 @@
             Student student1 = new("kåre", 19, "studiespesialisering", 99);
             Student student2 = new("oskar", 19, "studiespesialisering", 100);
 
-            //fagliste
-            Fag fag1 = new(301, "historie og filosofi", 30);
-            Fag fag2 = new(201, "historie", 15);
-            Fag fag3 = new(101, "filosofi", 15);
-            Fag fag4 = new(801, "programmering", 30);
-            Fag fag6 = new(303, "streetfigthing", 5);
-            Fag fag7 = new(505, "heimkunskap", 30);
-
             //karakterliste
-            Karakterer karakter1 = new(fag1, 5);
-            Karakterer karakter3 = new(fag4, 6);
-            Karakterer karakter4 = new(fag7, 3);
-            Karakterer karakter2 = new(fag2, 4);
-            Karakterer karakter5 = new(fag3, 2);
-            Karakterer karakter6 = new(fag6, 3);
+            Karakterer karakter1 = new(Fag.GetFag(0), 5);
+            Karakterer karakter3 = new(Fag.GetFag(3), 6);
+            Karakterer karakter4 = new(Fag.GetFag(5), 3);
+            Karakterer karakter2 = new(Fag.GetFag(1), 4);
+            Karakterer karakter5 = new(Fag.GetFag(2), 2);
+            Karakterer karakter6 = new(Fag.GetFag(4), 3);
 
             student1.run(karakter1, karakter3, karakter4);
             student2.run(karakter2, karakter5, karakter6);
